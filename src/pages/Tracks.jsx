c80df4269f6e732999562a695af70f3610d50258
Nawed Ali <nawed2611@gmail.com>
2022-11-23 18:53:21 +0530

    Add Tracks

--- src/pages/Tracks.jsx
+++ src/pages/Tracks.jsx
@@ -3,7 +3,7 @@ import trackssvg from '../assets/tracks.svg';
 
 const Tracks = () => {
     return (
-        <div className='flex Tracks justify-center items-center h-screen'>
+        <div className='flex flex-col Tracks justify-center items-center h-screen'>
             <div className='flex w-full space-x-4 px-20 mt-6'>
                 <div className='text-white font-semibold text-2xl'>
                     Hackathon Tracks
@@ -12,6 +12,42 @@ const Tracks = () => {
                 </div>
             </div>
             <img src={trackssvg} className='absolute object-cover w-screen h-screen -z-10' alt='Tracks' />
+            <div className=''>
+                <section className="overflow-hidden text-gray-700">
+                    <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
+                        <div className="flex flex-wrap -m-1 md:-m-2">
+                            <div className="flex flex-wrap w-1/2">
+                                <div className="w-1/2 p-1 md:p-2">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp" />
+                                </div>
+                                <div className="w-1/2 p-1 md:p-2">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp" />
+                                </div>
+                                <div className="w-full p-1 md:p-2">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
+                                </div>
+                            </div>
+                            <div className="flex flex-wrap w-1/2">
+                                <div className="w-full p-1 md:p-2">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
+                                </div>
+                                <div className="w-1/2 p-1 md:p-2">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
+                                </div>
+                                <div className="w-1/2 p-1 md:p-2">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
