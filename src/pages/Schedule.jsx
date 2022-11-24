e8e0ad211a3cc9fa93cfd14139b52b2fee60e2c1
Ansh Goyal <110151117+AnshGoyal27@users.noreply.github.com>
2022-11-24 13:24:23 +0530

    Update Schedule.jsx

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -2,7 +2,234 @@ import React from 'react'
 
 const Schedule = () => {
     return (
-        <div className='flex border justify-center items-center h-screen'>Schedule</div>
+        <div className='flex flex-col h-screen Schedule'>
+            <div className='flex w-full space-x-4 mt-6 px-20'>
+                <div className='font-bold text-2xl'>
+                    Schedule
+                </div>
+                <div className='bg-gradient-to-r from-[#2441A7] to-transparent w-[70%] h-2 mt-4'>
+                </div>
+            </div>
+
+            <br/>
+            <br/><br/>&nbsp;
+
+            <div className='flex flex-col w-full space-x-4 mt-6 px-20'>
+                <div>
+
+                </div>
+
+
+                <div className='flex flex-row items-center'>
+                    <div className='box p-4 bg-gradient-to-r from-red-700 to-rose-400 w-[10%]' style={{'marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%]' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1',}}>
+                    </div> 
+                    <div className='box p-4 bg-gradient-to-r from-rose-600 to-orange-600 w-[40%]' style={{'marginLeft':'-2.5vw','marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-gradient-to-r from-orange-600 to-orange-500 w-[40%]' style={{'marginRight':'-2.5vw','marginLeft':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-gradient-to-r from-orange-500 to-orange-400 w-[10%]' style={{'marginLeft':'-2.5vw','marginRight':'0px'}}>
+                    </div>
+                    <div className='box rounded-tr-full p-4 bg-gradient-to-r from-orange-400 to-orange-400 w-[3.5%]' style={{'marginLeft':'-1px'}}>
+                    </div>
+                </div>
+
+
+                <div className='flex flex-row items-center'>
+                    <div className='box p-4 bg-white w-[10%]' style={{'marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%]' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginLeft':'-2.5vw','marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginRight':'-2.5vw','marginLeft':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4bg-white w-[10%]' style={{'marginLeft':'-2.5vw','marginRight':'0px'}}>
+                    </div>
+                    <div className='box p-4 bg-gradient-to-b from-orange-400 to-orange-300 w-[3.5%]' style={{'marginLeft':'-1px'}}>
+                    </div>
+                </div>
+                <div className='flex flex-row items-center'>
+                    <div className='box p-4 bg-white w-[10%]' style={{'marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%]' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginLeft':'-2.5vw','marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginRight':'-2.5vw','marginLeft':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4bg-white w-[10%]' style={{'marginLeft':'-2.5vw','marginRight':'0px'}}>
+                    </div>
+                    <div className='box p-4 bg-gradient-to-b from-orange-300 to-yellow-200 w-[3.5%]' style={{'marginLeft':'-1px'}}>
+                    </div>
+                </div>
+                <div className='flex flex-row items-center'>
+                    <div className='box p-4 bg-white w-[10%]' style={{'marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%]' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginLeft':'-2.5vw','marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginRight':'-2.5vw','marginLeft':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4bg-white w-[10%]' style={{'marginLeft':'-2.5vw','marginRight':'0px'}}>
+                    </div>
+                    <div className='box p-4 bg-gradient-to-b from-yellow-200 to-yellow-200 w-[3.5%]' style={{'marginLeft':'-1px'}}>
+                    </div>
+                </div>
+                <div className='flex flex-row items-center'>
+                    <div className='box p-4 bg-white w-[10%]' style={{'marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%]' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginLeft':'-2.5vw','marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginRight':'-2.5vw','marginLeft':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4bg-white w-[10%]' style={{'marginLeft':'-2.5vw','marginRight':'0px'}}>
+                    </div>
+                    <div className='box p-4 bg-gradient-to-b from-yellow-200 to-yellow-300 w-[3.5%]' style={{'marginLeft':'-1px'}}>
+                    </div>
+                </div>
+
+
+                <div className='flex flex-row items-center'>
+                    <div className='box rounded-tl-full p-4 bg-gradient-to-l from-cyan-400 to-cyan-400 w-[10%]' style={{'marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%]' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-gradient-to-l from-green-300 to-cyan-400 w-[40%]' style={{'marginLeft':'-2.5vw','marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-gradient-to-l from-yellow-200 to-green-300 w-[40%]' style={{'marginRight':'-2.5vw','marginLeft':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-gradient-to-l from-yellow-300 to-yellow-200 w-[10%]' style={{'marginLeft':'-2.5vw','marginRight':'0px'}}>
+                    </div>
+                    <div className='box rounded-br-full p-4 bg-gradient-to-l from-yellow-300 to-yellow-300 w-[3.5%]' style={{'marginLeft':'0px'}}>
+                    </div>
+                </div>
+
+                <div className='flex flex-row-reverse items-center'>
+                    <div className='box p-4 bg-white w-[10%]' style={{'marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%]' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginLeft':'-2.5vw','marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginRight':'-2.5vw','marginLeft':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4bg-white w-[10%]' style={{'marginLeft':'-2.5vw','marginRight':'0px'}}>
+                    </div>
+                    <div className='box p-4 bg-gradient-to-b from-cyan-400 to-cyan-300 w-[3.5%]' style={{'marginLeft':'0px'}}>
+                    </div>
+                </div>
+                <div className='flex flex-row-reverse items-center'>
+                    <div className='box p-4 bg-white w-[10%]' style={{'marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%]' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginLeft':'-2.5vw','marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginRight':'-2.5vw','marginLeft':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4bg-white w-[10%]' style={{'marginLeft':'-2.5vw','marginRight':'0px'}}>
+                    </div>
+                    <div className='box p-4 bg-gradient-to-b from-cyan-300 to-cyan-300 w-[3.5%]' style={{'marginLeft':'0px'}}>
+                    </div>
+                </div>
+                <div className='flex flex-row-reverse items-center'>
+                    <div className='box p-4 bg-white w-[10%]' style={{'marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%]' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginLeft':'-2.5vw','marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginRight':'-2.5vw','marginLeft':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[10%]' style={{'marginLeft':'-2.5vw','marginRight':'0px'}}>
+                    </div>
+                    <div className='box p-4 bg-gradient-to-b from-cyan-300 to-blue-200 w-[3.5%]' style={{'marginLeft':'0px'}}>
+                    </div>
+                </div>
+                <div className='flex flex-row-reverse items-center'>
+                    <div className='box p-4 bg-white w-[10%]' style={{'marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%]' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginLeft':'-2.5vw','marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-white w-[40%]' style={{'marginRight':'-2.5vw','marginLeft':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4bg-white w-[10%]' style={{'marginLeft':'-2.5vw','marginRight':'0px'}}>
+                    </div>
+                    <div className='box p-4 bg-gradient-to-b from-blue-200 to-blue-300 w-[3.5%]' style={{'marginLeft':'0px'}}>
+                    </div>
+                </div>
+
+
+                <div className='flex flex-row items-center'>
+                    <div className='box rounded-bl-full p-4 bg-gradient-to-r from-blue-300 to-blue-300 w-[10%]' style={{'marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%]' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-gradient-to-r from-blue-300 to-purple-300 w-[40%]' style={{'marginLeft':'-2.5vw','marginRight':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-gradient-to-r from-purple-300 to-purple-500 w-[40%]' style={{'marginRight':'-2.5vw','marginLeft':'-2.5vw'}}>
+                    </div>
+                    <div className='box p-4 bg-white w-[6%] ' style={{'borderRadius':'2.5vw 2.5vw 2.5vw 2.5vw','zIndex':'1'}}>
+                    </div> 
+                    <div className='box p-4 bg-gradient-to-r from-purple-500 to-purple-600 w-[10%]' style={{'marginLeft':'-2.5vw','marginRight':'0px'}}>
+                    </div>
+                    <div className='box p-4 bg-gradient-to-r from-purple-600 to-purple-600 w-[3.5%]' style={{'marginLeft':'-1px'}}>
+                    </div>
+                </div>
+
+            </div>
+            
+            
+        </div>
     )
 }
 
