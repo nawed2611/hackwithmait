58554af92bd8053bf83a803577cb17c67f0490fb
nawed2611 <nawed2611@gmail.com>
2023-08-28 20:56:40 +0530

    Add changes

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -127,26 +127,6 @@ const FAQ = () => {
                     </div>
                 </div>
 
-                <hr className='bg-emerald-400 w-[100%] h-1 mt-4 mb-2' />
-
-                <div className='w-full '>
-                    {/* HEAD */}
-                    <div className='transition-all duration-1000 flex justify-between items-center  px-2'>
-                        <h1 className='font-bold text-xl py-2'>Accordion 1</h1>
-                        <button className='p-3 text-2xl text-black bg-white  rounded-full hover:bg-emerald-400 shadow-2xl border' style={{ 'transform': 'rotate(0deg)', 'transitionDuration': '1s' }} onClick={() => view('6')} id='6'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                            </svg>
-                        </button>
-
-                    </div>
-                    {/* CONTENT */}
-                    <div className='max-h-0 transition-all duration-1000 overflow-hidden' id='6c'>
-                        <p className='font-semibold p-2 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea incidunt molestiae magnam ab, unde facilis ipsam laborum repellat, doloremque quasi autem at voluptatibus labore nisi dolor dolore temporibus ipsa praesentium.
-                        </p>
-                    </div>
-                </div>
-
                 <hr className='bg-emerald-400 w-[100%] h-1 mt-4' />
             </div>
         </div>
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className='flex content-center Contact Us'>
             <div className="radial-bg-2 w-[98vw] h-[30vh] absolute"></div>
-            <div className='flex flex-wrap  mx-auto shrink justify-evenly'
+            <div className='flex flex-wrap  mx-auto shrink justify-between w-full sm:px-24'
                 style={{
                     'color': 'white',
                     'minHeight': '0',
@@ -13,7 +13,7 @@ const Footer = () => {
                 <div className='flex flex-wrap flex-col' id='boxes'>
                     <div className='flex flex-col'>
                         <h1 className='text-4xl'>Date</h1>
-                        <p className='text-xl'>22nd & 23rd December 2022</p>
+                        <p className='text-xl'>22nd & 23rd November 2023</p>
                     </div> &nbsp;
                     <div className='flex flex-col'>
                         <h1 className='text-4xl'>Venue</h1>
@@ -38,11 +38,6 @@ const Footer = () => {
                             <a href='https://in.linkedin.com/company/techcommait' ><img className='invert' src='https://img.icons8.com/ios/512/linkedin.png' style={{ 'maxWidth': '30px' }} /></a>
                         </div>
                     </div>
-                </div>
-                <div className='flex flex-col' id='boxes'>
-                    <h1 className='text-4xl'>Resources</h1>
-                    <p className='text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum earum rem sed, ut tenetur iure commodi minus, sapiente maiores odio alias non est vitae, accusantium nemo. Voluptate, repudiandae. Error, in.
-                    </p>
                 </div>
             </div>
 
