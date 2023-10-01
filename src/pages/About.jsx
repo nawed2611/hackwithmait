955030a05fb49ac05656d3f40d9c3b1adec36401
nawed2611 <nawed2611@gmail.com>
2023-10-01 21:04:48 +0530

    Update texts

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,21 +13,21 @@ const About = () => {
       </div>
       <div className='radial-bg w-[98vw] h-screen'></div>
       <div className='flex glass flex-col justify-center items-center m-2 sm:m-16 mt-8 p-6'>
-        <p className='leading-relaxed text-xl font-[500]'>
+        <p className='leading-relaxed text-3xl font-[500]'>
           Welcome to HackwithMAIT3.0, the annual hackathon organized by the Computer Science and Engineering department at MAIT.
 
           This event brings together some of the brightest and most creative minds from across India to compete and collaborate on innovative ideas and projects. Over the course of the hackathon, participants will have the opportunity to work on a wide range of challenges and problem statements, and to learn from experienced mentors and judges from the tech industry.
 
-          At HackwithMAIT3.0, we believe that creativity and ingenuity knows no bounds. Whether you are a seasoned programmer or a complete beginner, we welcome all participants who are eager to learn and to push the boundaries of what is possible.
+          At HackwithMAIT 4.0, we believe that creativity and ingenuity knows no bounds. Whether you are a seasoned programmer or a complete beginner, we welcome all participants who are eager to learn and to push the boundaries of what is possible.
 
-          We look forward to seeing the amazing ideas and solutions that emerge from this year's hackathon, and to celebrating the achievements of all our participants. Join us and let's make HackwithMAIT3.0 a truly memorable hackathon!
+          We look forward to seeing the amazing ideas and solutions that emerge from this year's hackathon, and to celebrating the achievements of all our participants. Join us and let's make HackwithMAIT 4.0 a truly memorable hackathon!
         </p>
 
         <div className='flex flex-col sm:flex-row mt-12 w-full p-2 sm:px-6 justify-between text-xl font-[800]'>
           <div className=''>
             <p className='underline mb-2'>Date</p>
             <p>22nd-23rd November 2023 - Hacking</p>
-            <p>23rd November 2023 - Development Pitching</p>
+            <p>23rd November 2023 - Project Pitching</p>
           </div>
           <div className='mt-2 sm:mt-0'>
             <p className='underline mb-2'>Venue</p>
