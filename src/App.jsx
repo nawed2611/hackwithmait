f409fc68ab99b64bf08664d56d8b999426ed0003
Aviral jain <aviral2853@gmail.com>
2023-10-19 09:21:31 +0530

    initial changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,16 @@ import Sponsors from './pages/Sponsors';
 import FAQ from './pages/FAQ';
 import Team from './pages/Team';
 import Footer from './pages/Footer';
-
+import Timeline from './pages/Timeline';
 function App() {
   return (
-    
     <div className="flex flex-col overflow-x-hidden ">
       <Hero />
       <About />
       <CarouselScreen />
       {/* <Speakers /> */}
       <Tracks />
+      <Timeline/>
       {/* <Schedule /> */}
       <Sponsors />
       {/* <Team /> */}
