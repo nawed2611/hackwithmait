cea14166fe7cd216d5c3b1c5331f5c13124252f2
aman <amansharma12607@gmail.com>
2023-10-27 13:26:43 +0530

    fixing bugs a1

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Table from './pages/innovative_table'
-import SpTable from './pages/sponser_table'
+import SPTable from './pages/sponser_table'
+
 import './index.css'
 import 'tw-elements';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SpTable from './pages/sponser_table'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
