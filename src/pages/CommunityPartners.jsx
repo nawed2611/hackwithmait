c5d47e3fed0c515f6d68ed70584959367d582c50
exploring-solver <amansharma12607@gmail.com>
2023-11-18 23:24:30 +0530

    kwikpic 2

--- src/pages/CommunityPartners.jsx
+++ src/pages/CommunityPartners.jsx
@@ -1,9 +1,10 @@
 import OCD from '../Community_Partners/OCD.png'
 import Techcom from '../Community_Partners/techcom.jpg'
 import WAC from '../Community_Partners/WAC.png'
+import Kwikpic from '../assets/Sponsors/kwikpic_partner.jpg'
 
 
-const Sponsors = () => {
+const CommunityPartners = () => {
     return (
         <div className='flex flex-col carousel-bg Sponsors py-1'>
 
@@ -31,8 +32,25 @@ const Sponsors = () => {
                 </div>
  
             </div>
+            <div className='flex w-full space-x-4 px-20 mt-14'>
+                <div className='text-white font-semibold text-2xl'>
+                    Photo Sharing Partner
+                </div>
+                <div className='bg-gradient-to-r from-white to-transparent w-full h-2 mt-4'>
+                </div>
+            </div>
+
+            <div className='radial-bg w-[98vw] h-screen absolute -z-10'></div>
+
+            {/* sponsors row 1 */}
+            <div className='flex flex-wrap py-4 justify-center'>
+                <div className='flex px-12 my-5 gap-3 items-center justify-center mx-10 max-md:p-0 max-md:my-3'>
+                    <img src={Kwikpic} alt="" className=' h-24' />
+                </div>
+ 
+            </div>
         </div>
     )
 }
 
-export default Sponsors
+export default CommunityPartners
